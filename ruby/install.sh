b61f09441d83238b9643d680c5585b86c04970d2
Josh Bielick <jbielick@gmail.com>
2017-11-19 08:51:07 -0500

    allow brew bundle to fail, rvm idempotency

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,2 +1,8 @@
-echo "Installing RVM..."
-curl -sSL https://get.rvm.io | bash -s stable --ruby
+#!/bin/sh
+if test ! $(which rvm)
+then
+  echo "Installing RVM..."
+  curl -sSL https://get.rvm.io | bash -s stable --ruby
+fi
+
+exit 0
