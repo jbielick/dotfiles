62dbea681088869c6c12cf64d14c213448a4e7fa
Josh Bielick <jbielick@gmail.com>
2019-02-25 15:51:42 -0500

    add kns alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,5 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias vssh='vagrant ssh'
 
 alias k=kubectl
+
+alias kns="kubectl config set-context $(kubectl config current-context) --namespace "
